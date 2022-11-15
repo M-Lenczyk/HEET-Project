6dca50b2cc25aea870d96475ed308e34805a4b08
M-Lenczyk <michal.lenczyk@student.put.poznan.pl>
2022-11-15 04:24:48 +0100

    ffs kill me
    Long road ahead 4 me

---
+++ build/CMakeFiles/simple-integers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple-integers.dir/simple-integers.cpp.o"
+  "simple-integers"
+  "simple-integers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple-integers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
