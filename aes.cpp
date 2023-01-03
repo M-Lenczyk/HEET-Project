2b73740762bdd3e08d71b9115cf74dc35098948a
M-Lenczyk <michal.lenczyk@student.put.poznan.pl>
2023-01-03 03:33:10 +0100

    aes added input stream & microsecond timer

--- aes.cpp
+++ aes.cpp
@@ -1,5 +1,7 @@
 #include <vector>
 #include <iostream>
+#include <sstream>
+#include <cstdlib>
 
 #include <crypto++/cryptlib.h>
 #include <crypto++/hrtimer.h>
@@ -9,26 +11,6 @@
 #include <crypto++/filters.h>
 #include <crypto++/osrng.h>
 #include <crypto++/base64.h>
-
-std::string int_vector_to_string(const std::vector<unsigned short> &v1)
-{
-    std::string res = "";
-    for(const auto &x : v1)
-    {
-        res += (char)x;
-    }
-    return res;
-}
-
-std::vector<unsigned short> string_to_int_vector(const std::string &s)
-{
-    std::vector<unsigned short> res;
-    for(const auto &x : s)
-    {
-        res.push_back((unsigned short)x);
-    }
-    return res;
-}
 
 std::string encrypt(const std::string& input, const std::vector<uint8_t>& key, const std::vector<uint8_t>& iv) {
     std::string cipher;
@@ -69,31 +51,20 @@ std::string decrypt(const std::string& cipher_text, const std::vector<uint8_t>& 
 
     return plain_text;
 }
-
-int main()
+ 
+void experimentAES(std::string input, std::size_t AES_KEY_SIZE, CryptoPP::Timer timer)
 {
-
-	//CryptoPP::ThreadUserTimer timerBase(CryptoPP::TimerBase::Unit unit = CryptoPP::TimerBase::MILLISECONDS, bool stuckAtZero = false);
-    double elapsedTime;
-    
-    static constexpr size_t AES_KEY_SIZE_128 = 128 / 8; //AES-128
-    static constexpr size_t AES_KEY_SIZE_192 = 192 / 8; //AES-192
-    static constexpr size_t AES_KEY_SIZE_256 = 256 / 8; //AES-256
-   
-    const std::string input {"123456789"};
-    std::cout<<"\nINPUT-DATA: (Before Encryption): "<<input<<std::endl;
-    elapsedTime = 0;
-	CryptoPP::Timer timer(CryptoPP::Timer::MILLISECONDS);
+    double elapsedTime=0.0;
+	
+	//Generowanie kluczy
 	timer.StartTimer();
 	
-    std::vector<uint8_t> key(AES_KEY_SIZE_128);
+    std::vector<uint8_t> key(AES_KEY_SIZE);
     std::vector<uint8_t> iv(CryptoPP::AES::BLOCKSIZE);
+
+    elapsedTime = timer.ElapsedTimeAsDouble();
+    std::cout<<"\nKey generation time for "<<AES_KEY_SIZE*8<<" bit key (us): "<<elapsedTime<<std::endl;
     
-	elapsedTime = timer.ElapsedTimeAsDouble();
-	
-	std::cout<<"\nKey generation (ms): "<<elapsedTime<<std::endl;
-	
-	elapsedTime = 0;
 	timer.StartTimer();
 	
     CryptoPP::BlockingRng rand;
@@ -101,30 +72,64 @@ int main()
     rand.GenerateBlock(iv.data(), iv.size());
     
 	elapsedTime = timer.ElapsedTimeAsDouble();
+	std::cout<<"\nInitialization vector and block generation time (us): "<<elapsedTime<<std::endl;
 	
-	std::cout<<"\nInitialization vector and block generation time (ms): "<<elapsedTime<<std::endl;
 	
-	elapsedTime = 0;
 	timer.StartTimer();
 	
     auto cipher = encrypt(input, key, iv);
     
     elapsedTime = timer.ElapsedTimeAsDouble();
-	
-	std::cout<<"\nEncryption time (ms): "<<elapsedTime<<std::endl;
+	std::cout<<"\nEncryption time (us): "<<elapsedTime<<std::endl;
     
-    elapsedTime = 0;
+    
 	timer.StartTimer();
 	
     auto plain_text = decrypt(cipher, key, iv);
     
-    elapsedTime = timer.ElapsedTimeAsDouble();
-	
-	std::cout<<"\nDecryption time (ms): "<<elapsedTime<<std::endl;
+	elapsedTime = timer.ElapsedTimeAsDouble();
+	std::cout<<"\nDecryption time (us): "<<elapsedTime<<std::endl;
 
-    if(plain_text != input) {
+    if(plain_text != input) 
+	{
         std::cout << "\nData after Encryption & Decryption is diffrent!!!" << std::endl;
     }
-	std::cout<<"\nRESULT (After Decryption): "<<plain_text<<std::endl;
-    return 0;
+	//std::cout<<"\nRESULT (After Decryption): "<<plain_text<<std::endl;
+	
+}
+int main()
+{	
+
+	//CryptoPP::ThreadUserTimer timerBase(CryptoPP::TimerBase::Unit unit = CryptoPP::TimerBase::MILLISECONDS, bool stuckAtZero = false);
+    
+    constexpr size_t AES_KEY_SIZE_128 = 16; //AES-128
+    constexpr size_t AES_KEY_SIZE_192 = 24; //AES-192
+    constexpr size_t AES_KEY_SIZE_256 = 32; //AES-256
+    
+    //Zegar, precyzja 
+    CryptoPP::Timer timer(CryptoPP::Timer::MICROSECONDS);
+    
+   	std::vector<unsigned short int> dataSetVector;//10x10
+   	const short int vectorSize = 10;
+   	const short int numberOfVectors = 10;
+   	const int fullSize = vectorSize*numberOfVectors;
+   	
+   	std::stringstream inputStream;
+   	srand((unsigned) time(NULL));
+   	
+   	for(int i = 0; i<fullSize;i++)
+   	{
+   		unsigned short int sampleValue = (rand()%10)+1;
+   		dataSetVector.push_back(sampleValue);
+   		inputStream<<dataSetVector[i];
+	}
+	//Dataset Loading
+    const std::string input = inputStream.str();
+    //std::cout<<"\nINPUT-DATA-STRING: (Before Encryption): "<<input<<std::endl;
+    
+    experimentAES(input,AES_KEY_SIZE_128,timer);
+    experimentAES(input,AES_KEY_SIZE_192,timer);
+	experimentAES(input,AES_KEY_SIZE_256,timer);
+ 
+	return 0;
 }
